451d325bfc0d96da53e93f3c481ce2fc3f439acf
IyumDeepak <115373833+IyumDeepak@users.noreply.github.com>
2023-01-27 00:52:35 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,51 @@
 import React from 'react';
 import './App.css';
 import { SheetDirective, SheetsDirective, SpreadsheetComponent, ColumnsDirective, ColumnDirective, 
-    RangeDirective, RangesDirective, getFormatFromType, RowsDirective, RowDirective, CellsDirective, CellDirective} 
-    from '@syncfusion/ej2-react-spreadsheet';
-import {protectSheetData} from './data';
+    RangeDirective, RangesDirective, getFormatFromType, RowsDirective, RowDirective, CellsDirective, CellDirective, ChartModel, getCell, getRangeIndexes} from '@syncfusion/ej2-react-spreadsheet';
+import {GDPData} from './data';
 function App() {
-  let ssObj: SpreadsheetComponent;
-  let protectOptions: object = {
-    selectCells: true
-  }  
+  let ssObj: SpreadsheetComponent;  
+  let chartDetails: ChartModel[] = [{type:"Line", theme:"Fabric", isSeriesInRows: false, range: "A2:E8" }];
   const onCreated=()=>{
     // Formatting cells dynamically using cellFormat method
-    ssObj.cellFormat({ backgroundColor: '#e56590', color: '#fff', fontWeight: 'bold', textAlign: 'center' }, 'B1');
-    ssObj.protectSheet(1, {selectCells:true}, "Syncfusion");
-    // ssObj.unprotectSheet(0);
-    ssObj.lockCells("B1:C9", false);
+    ssObj.cellFormat({ backgroundColor: '#e56590', color: '#fff', fontWeight: 'bold', textAlign: 'center' }, 'A1:E1');
+    // Applying currency format to the specified range.
+    ssObj.numberFormat(getFormatFromType('Currency'), 'B2:E8');
+
+    //Insert chart through Spreadsheet method.
+    // ssObj.insertChart(chartDetails);
+
+    setTimeout(function(){
+      let rangeIndex = getRangeIndexes("G1");
+      let cell = getCell(rangeIndex[0], rangeIndex[1], ssObj.getActiveSheet());
+
+      if(cell.chart){
+        ssObj.deleteChart(cell.chart[0].id);
+      }
+    }, 2000)
   }
   return (
     <div className="App">
-      <SpreadsheetComponent ref={((s:SpreadsheetComponent)=>ssObj=s)} isProtected={true} password="Syncfusion"
-        height={560}  created={onCreated}>
+      <SpreadsheetComponent ref={((s:SpreadsheetComponent)=>ssObj=s)}
+        height={560}  created={onCreated} allowChart={true}>
         <SheetsDirective>
-          <SheetDirective name='EMI Calculator'>    {/*protectSettings={protectOptions} */}
+            <SheetDirective name='GDP'>
               <RowsDirective>
-                  <RowDirective>
-                      <CellsDirective>
-                          <CellDirective index={1} value='Home Loan Calculator' colSpan={2}></CellDirective>
-                      </CellsDirective>
-                  </RowDirective>
-                  <RowDirective>
-                      <CellsDirective>
-                          <CellDirective index={1} value='Loan Amount:'></CellDirective>
-                          <CellDirective value='100000' format={getFormatFromType('Currency')} ></CellDirective>
-                      </CellsDirective>
-                  </RowDirective>
-                  <RowDirective>
-                      <CellsDirective>
-                          <CellDirective index={1} value='Interest Rate:'></CellDirective>
-                          <CellDirective value='0.08' format={getFormatFromType('Percentage')}></CellDirective>
-                      </CellsDirective>
-                  </RowDirective>
-                  <RowDirective>
-                      <CellsDirective>
-                          <CellDirective index={1} value='Periods (terms in year):'></CellDirective>
-                          <CellDirective value='1'></CellDirective>
-                      </CellsDirective>
-                  </RowDirective>
-                  <RowDirective>
-                      <CellsDirective>
-                          <CellDirective index={1} value='Start Date:'></CellDirective>
-                          <CellDirective value='03-03-2020'></CellDirective>
-                      </CellsDirective>
-                  </RowDirective>
-                  <RowDirective>
-                      <CellsDirective>
-                          <CellDirective index={1} value='Loan EMI:'></CellDirective>
-                          <CellDirective value='8698.84' format={getFormatFromType('Currency')}></CellDirective>
-                      </CellsDirective>
-                  </RowDirective>
-                  <RowDirective>
-                      <CellsDirective>
-                          <CellDirective index={1} value='Number of Payments:'></CellDirective>
-                          <CellDirective value='12'></CellDirective>
-                      </CellsDirective>
-                  </RowDirective>
-                  <RowDirective>
-                      <CellsDirective>
-                          <CellDirective index={1} value='Total Repayment Amount:'></CellDirective>
-                          <CellDirective value='104386.11' format={getFormatFromType('Currency')}></CellDirective>
-                      </CellsDirective>
-                  </RowDirective>
-                  <RowDirective>
-                      <CellsDirective>
-                          <CellDirective index={1} value='Total Interest Amount:'></CellDirective>
-                          <CellDirective value='4386.11' format={getFormatFromType('Currency')}></CellDirective>
-                      </CellsDirective>
-                  </RowDirective>
+                <RowDirective index={0}>
+                  <CellsDirective>
+                    <CellDirective index={6} chart={chartDetails}></CellDirective>
+                  </CellsDirective>
+                </RowDirective>
               </RowsDirective>
-              <ColumnsDirective>
-                  <ColumnDirective index={1} width={190}></ColumnDirective>
-                  <ColumnDirective width={100}></ColumnDirective>
-              </ColumnsDirective>
-          </SheetDirective>
-          <SheetDirective name='EMI Schedule'>
               <RangesDirective>
-                  <RangeDirective dataSource={protectSheetData}></RangeDirective>
+                  <RangeDirective dataSource={GDPData}></RangeDirective>
               </RangesDirective>
               <ColumnsDirective>
-                  <ColumnDirective index={1} width={110}></ColumnDirective>
-                  <ColumnDirective width={85}></ColumnDirective>
-                  <ColumnDirective width={85}></ColumnDirective>
                   <ColumnDirective width={80}></ColumnDirective>
-                  <ColumnDirective width={90}></ColumnDirective>
+                  <ColumnDirective width={75}></ColumnDirective>
+                  <ColumnDirective width={75}></ColumnDirective>
+                  <ColumnDirective width={75}></ColumnDirective>
+                  <ColumnDirective width={75}></ColumnDirective>
               </ColumnsDirective>
             </SheetDirective>
         </SheetsDirective>
@@ -101,3 +55,107 @@ function App() {
 }
 
 export default App;
+
+// import React from 'react';
+// import './App.css';
+// import { SheetDirective, SheetsDirective, SpreadsheetComponent, ColumnsDirective, ColumnDirective, 
+//     RangeDirective, RangesDirective, getFormatFromType, RowsDirective, RowDirective, CellsDirective, CellDirective} 
+//     from '@syncfusion/ej2-react-spreadsheet';
+// import {protectSheetData} from './data';
+// function App() {
+//   let ssObj: SpreadsheetComponent;
+//   let protectOptions: object = {
+//     selectCells: true
+//   }  
+//   const onCreated=()=>{
+//     // Formatting cells dynamically using cellFormat method
+//     ssObj.cellFormat({ backgroundColor: '#e56590', color: '#fff', fontWeight: 'bold', textAlign: 'center' }, 'B1');
+//     ssObj.protectSheet(1, {selectCells:true}, "Syncfusion");
+//     // ssObj.unprotectSheet(0);
+//     ssObj.lockCells("B1:C9", false);
+//   }
+//   return (
+//     <div className="App">
+//       <SpreadsheetComponent ref={((s:SpreadsheetComponent)=>ssObj=s)} isProtected={true} password="Syncfusion"
+//         height={560}  created={onCreated}>
+//         <SheetsDirective>
+//           <SheetDirective name='EMI Calculator'>    {/*protectSettings={protectOptions} */}
+//               <RowsDirective>
+//                   <RowDirective>
+//                       <CellsDirective>
+//                           <CellDirective index={1} value='Home Loan Calculator' colSpan={2}></CellDirective>
+//                       </CellsDirective>
+//                   </RowDirective>
+//                   <RowDirective>
+//                       <CellsDirective>
+//                           <CellDirective index={1} value='Loan Amount:'></CellDirective>
+//                           <CellDirective value='100000' format={getFormatFromType('Currency')} ></CellDirective>
+//                       </CellsDirective>
+//                   </RowDirective>
+//                   <RowDirective>
+//                       <CellsDirective>
+//                           <CellDirective index={1} value='Interest Rate:'></CellDirective>
+//                           <CellDirective value='0.08' format={getFormatFromType('Percentage')}></CellDirective>
+//                       </CellsDirective>
+//                   </RowDirective>
+//                   <RowDirective>
+//                       <CellsDirective>
+//                           <CellDirective index={1} value='Periods (terms in year):'></CellDirective>
+//                           <CellDirective value='1'></CellDirective>
+//                       </CellsDirective>
+//                   </RowDirective>
+//                   <RowDirective>
+//                       <CellsDirective>
+//                           <CellDirective index={1} value='Start Date:'></CellDirective>
+//                           <CellDirective value='03-03-2020'></CellDirective>
+//                       </CellsDirective>
+//                   </RowDirective>
+//                   <RowDirective>
+//                       <CellsDirective>
+//                           <CellDirective index={1} value='Loan EMI:'></CellDirective>
+//                           <CellDirective value='8698.84' format={getFormatFromType('Currency')}></CellDirective>
+//                       </CellsDirective>
+//                   </RowDirective>
+//                   <RowDirective>
+//                       <CellsDirective>
+//                           <CellDirective index={1} value='Number of Payments:'></CellDirective>
+//                           <CellDirective value='12'></CellDirective>
+//                       </CellsDirective>
+//                   </RowDirective>
+//                   <RowDirective>
+//                       <CellsDirective>
+//                           <CellDirective index={1} value='Total Repayment Amount:'></CellDirective>
+//                           <CellDirective value='104386.11' format={getFormatFromType('Currency')}></CellDirective>
+//                       </CellsDirective>
+//                   </RowDirective>
+//                   <RowDirective>
+//                       <CellsDirective>
+//                           <CellDirective index={1} value='Total Interest Amount:'></CellDirective>
+//                           <CellDirective value='4386.11' format={getFormatFromType('Currency')}></CellDirective>
+//                       </CellsDirective>
+//                   </RowDirective>
+//               </RowsDirective>
+//               <ColumnsDirective>
+//                   <ColumnDirective index={1} width={190}></ColumnDirective>
+//                   <ColumnDirective width={100}></ColumnDirective>
+//               </ColumnsDirective>
+//           </SheetDirective>
+//           <SheetDirective name='EMI Schedule'>
+//               <RangesDirective>
+//                   <RangeDirective dataSource={protectSheetData}></RangeDirective>
+//               </RangesDirective>
+//               <ColumnsDirective>
+//                   <ColumnDirective index={1} width={110}></ColumnDirective>
+//                   <ColumnDirective width={85}></ColumnDirective>
+//                   <ColumnDirective width={85}></ColumnDirective>
+//                   <ColumnDirective width={80}></ColumnDirective>
+//                   <ColumnDirective width={90}></ColumnDirective>
+//               </ColumnsDirective>
+//             </SheetDirective>
+//         </SheetsDirective>
+//       </SpreadsheetComponent>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/data.js
+++ src/data.js
@@ -1,99 +1,152 @@
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.protectSheetData = [
+exports.GDPData = [
     {
-        "Tenure": "1",
-        "Payment Date": "03-04-2020",
-        "EMI": "8698.84",
-        "Prinicpal": "8032.18",
-        "Interest": "666.67",
-        "Balance": "91967.82"
+        "Country": "USA",
+        "Year 2017": "18.00",
+        "Year 2018": "20.40",
+        "Year 2019": "21.42",
+        "Year 2020": "19.48"
     },
     {
-        "Tenure": "2",
-        "Payment Date": "03-05-2020",
-        "EMI": "8698.84",
-        "Prinicpal": "8085.72",
-        "Interest": "613.12",
-        "Balance": "83882.10"
+        "Country": "China",
+        "Year 2017": "11.00",
+        "Year 2018": "14.00",
+        "Year 2019": "14.32",
+        "Year 2020": "12.23"
     },
     {
-        "Tenure": "3",
-        "Payment Date": "03-06-2020",
-        "EMI": "8698.84",
-        "Prinicpal": "8139.63",
-        "Interest": "559.21",
-        "Balance": "75742.47"
+        "Country": "Japan",
+        "Year 2017": "4.4",
+        "Year 2018": "5.10",
+        "Year 2019": "5.08",
+        "Year 2020": "4.87"
     },
     {
-        "Tenure": "4",
-        "Payment Date": "03-07-2020",
-        "EMI": "8698.84",
-        "Prinicpal": "8193.89",
-        "Interest": "504.95",
-        "Balance": "67548.58"
+        "Country": "Germany",
+        "Year 2017": "3.3",
+        "Year 2018": "4.20",
+        "Year 2019": "3.84",
+        "Year 2020": "3.69"
     },
     {
-        "Tenure": "5",
-        "Payment Date": "03-08-2020",
-        "EMI": "8698.84",
-        "Prinicpal": "8248.52",
-        "Interest": "450.32",
-        "Balance": "59300.06"
+        "Country": "India",
+        "Year 2017": "2.00",
+        "Year 2018": "2.85",
+        "Year 2019": "2.87",
+        "Year 2020": "2.65"
     },
     {
-        "Tenure": "6",
-        "Payment Date": "03-09-2020",
-        "EMI": "8698.84",
-        "Prinicpal": "8303.51",
-        "Interest": "395.33",
-        "Balance": "50996.55"
+        "Country": "UK",
+        "Year 2017": "2.90",
+        "Year 2018": "2.94",
+        "Year 2019": "2.82",
+        "Year 2020": "2.63"
     },
     {
-        "Tenure": "7",
-        "Payment Date": "03-10-2020",
-        "EMI": "8698.84",
-        "Prinicpal": "8358.87",
-        "Interest": "339.98",
-        "Balance": "42637.68"
-    },
-    {
-        "Tenure": "8",
-        "Payment Date": "03-11-2020",
-        "EMI": "8698.84",
-        "Prinicpal": "8414.59",
-        "Interest": "284.25",
-        "Balance": "34223.09"
-    },
-    {
-        "Tenure": "9",
-        "Payment Date": "03-12-2020",
-        "EMI": "8698.84",
-        "Prinicpal": "8470.69",
-        "Interest": "228.15",
-        "Balance": "25752.40"
-    },
-    {
-        "Tenure": "10",
-        "Payment Date": "03-01-2021",
-        "EMI": "8698.84",
-        "Prinicpal": "8527.16",
-        "Interest": "171.68",
-        "Balance": "17225.24"
-    },
-    {
-        "Tenure": "11",
-        "Payment Date": "03-02-2021",
-        "EMI": "8698.84",
-        "Prinicpal": "8584.01",
-        "Interest": "114.83",
-        "Balance": "8641.23"
-    },
-    {
-        "Tenure": "12",
-        "Payment Date": "03-03-2021",
-        "EMI": "8698.84",
-        "Prinicpal": "8641.23",
-        "Interest": "57.61",
-        "Balance": "0.00"
+        "Country": "France",
+        "Year 2017": "2.40",
+        "Year 2018": "2.93",
+        "Year 2019": "2.71",
+        "Year 2020": "2.58"
     }
 ];
+
+// Object.defineProperty(exports, "__esModule", { value: true });
+// exports.protectSheetData = [
+//     {
+//         "Tenure": "1",
+//         "Payment Date": "03-04-2020",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8032.18",
+//         "Interest": "666.67",
+//         "Balance": "91967.82"
+//     },
+//     {
+//         "Tenure": "2",
+//         "Payment Date": "03-05-2020",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8085.72",
+//         "Interest": "613.12",
+//         "Balance": "83882.10"
+//     },
+//     {
+//         "Tenure": "3",
+//         "Payment Date": "03-06-2020",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8139.63",
+//         "Interest": "559.21",
+//         "Balance": "75742.47"
+//     },
+//     {
+//         "Tenure": "4",
+//         "Payment Date": "03-07-2020",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8193.89",
+//         "Interest": "504.95",
+//         "Balance": "67548.58"
+//     },
+//     {
+//         "Tenure": "5",
+//         "Payment Date": "03-08-2020",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8248.52",
+//         "Interest": "450.32",
+//         "Balance": "59300.06"
+//     },
+//     {
+//         "Tenure": "6",
+//         "Payment Date": "03-09-2020",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8303.51",
+//         "Interest": "395.33",
+//         "Balance": "50996.55"
+//     },
+//     {
+//         "Tenure": "7",
+//         "Payment Date": "03-10-2020",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8358.87",
+//         "Interest": "339.98",
+//         "Balance": "42637.68"
+//     },
+//     {
+//         "Tenure": "8",
+//         "Payment Date": "03-11-2020",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8414.59",
+//         "Interest": "284.25",
+//         "Balance": "34223.09"
+//     },
+//     {
+//         "Tenure": "9",
+//         "Payment Date": "03-12-2020",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8470.69",
+//         "Interest": "228.15",
+//         "Balance": "25752.40"
+//     },
+//     {
+//         "Tenure": "10",
+//         "Payment Date": "03-01-2021",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8527.16",
+//         "Interest": "171.68",
+//         "Balance": "17225.24"
+//     },
+//     {
+//         "Tenure": "11",
+//         "Payment Date": "03-02-2021",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8584.01",
+//         "Interest": "114.83",
+//         "Balance": "8641.23"
+//     },
+//     {
+//         "Tenure": "12",
+//         "Payment Date": "03-03-2021",
+//         "EMI": "8698.84",
+//         "Prinicpal": "8641.23",
+//         "Interest": "57.61",
+//         "Balance": "0.00"
+//     }
+// ];
